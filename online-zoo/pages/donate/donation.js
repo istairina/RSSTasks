7a0d5e70456ff7f9bf80cf1e39c76f7e6fc5f882
Irina Migunova <ista.irina@gmail.com>
2022-10-07 22:11:04 +0300

    feat: finish js for Amount panel in Donate page

---
+++ online-zoo/pages/donate/donation.js
@@ -0,0 +1,27 @@
+const inputText = document.querySelector('.input-amount');
+const amountNumbers = document.querySelectorAll('input[name="choose-amount"]');
+
+
+function putNumberInInputText() {
+    for (const i of amountNumbers) {
+        if (i.checked) {
+            inputText.value = i.value;
+        }
+    }
+}
+
+function selectNumber() {
+    for (const i of amountNumbers) {
+        if (i.checked) {
+            i.checked = false;
+            break;
+        }
+    }
+
+    for (const p of amountNumbers) {
+        if (inputText.value == p.value) {
+            p.checked = true;
+            break;
+        }
+    }
+}
