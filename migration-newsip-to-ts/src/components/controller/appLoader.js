550b1cf059cae600bcd09f2d90c12ce9f77cfe06
Irina Migunova <ista.irina@gmail.com>
2022-12-12 21:23:47 +0300

    fix: fix proxy server for deploy

--- migration-newsip-to-ts/src/components/controller/appLoader.js
+++ migration-newsip-to-ts/src/components/controller/appLoader.js
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: '5dfcb388bf6c4cea863f39310339d47b', // получите свой ключ https://newsapi.org/
         });
     }
