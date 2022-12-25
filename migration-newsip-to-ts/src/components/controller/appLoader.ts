cd0d06f649efae3dbbbff100bf25fc1eb955e5c1
Irina Migunova <ista.irina@gmail.com>
2022-12-25 23:00:55 +0300

    fix: delete file from another project and change url to api

--- migration-newsip-to-ts/src/components/controller/appLoader.ts
+++ migration-newsip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi-redirect-production.up.railway.app/', {
+        super('https://newsapi.org/v2/', {
             apiKey: '5dfcb388bf6c4cea863f39310339d47b', // получите свой ключ https://newsapi.org/
         });
     }
