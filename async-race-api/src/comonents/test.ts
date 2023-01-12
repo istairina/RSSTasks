98867b5e2b8e6144e97826dbed80751ae08b95fd
Irina Migunova <ista.irina@gmail.com>
2023-01-12 05:07:45 +0300

    init: start project async-race

---
+++ async-race-api/src/comonents/test.ts
@@ -0,0 +1,9 @@
+class Test {
+  key: number = 123;
+
+  print() {
+    console.log(this.key);
+  }
+}
+
+export default Test;
---
+++ async-race-api/src/index.ts
@@ -0,0 +1,6 @@
+import Test from "../src/comonents/test";
+import "./sass/global.scss";
+
+const dr = new Test();
+dr.print();
+// console.log("test");
