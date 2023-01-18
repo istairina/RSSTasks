744e83f4797234c08ef67041623a8a5501c5193f
Irina Migunova <ista.irina@gmail.com>
2023-01-18 04:30:43 +0300

    feat: add model name to generated cars

---
+++ async-race-api/src/components/db/carModels.ts
@@ -0,0 +1,13 @@
+export const allModels = [
+  'first',
+  'best',
+  'prima',
+  'perfecto',
+  'amazing',
+  'solo',
+  'sporty',
+  'winner',
+  'selecto',
+  'superior',
+  'super',
+];
--- async-race-api/src/components/generateCarsServer.ts
+++ async-race-api/src/components/generateCarsServer.ts
@@ -1,11 +1,13 @@
-import { allBrands } from './carBrands';
+import { allBrands } from './db/carBrands';
+import { allModels } from './db/carModels';
 import addCar from './addCar';
 
 export default function GenerateCarsServer() {
   for (let i = 0; i < 100; i++) {
-    const randomIndex = Math.floor(Math.random() * (allBrands.length - 1));
+    const randomIndexBrands = Math.floor(Math.random() * (allBrands.length - 1));
+    const randomIndexModels = Math.floor(Math.random() * (allModels.length - 1));
     const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    console.log(`${randomIndex} ${randomColor}`);
-    addCar(allBrands[randomIndex], randomColor);
+    const nameCar = `${allBrands[randomIndexBrands]} ${allModels[randomIndexModels]}`;
+    addCar(nameCar, randomColor);
   }
 }
