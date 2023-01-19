4e4b61b7b6b26a4650abe5f10b14acb413ab8727
Irina Migunova <ista.irina@gmail.com>
2023-01-19 16:19:42 +0300

    feat: new setup webpack

--- async-race-api/webpack.config.js
+++ async-race-api/webpack.config.js
@@ -1,67 +1,67 @@
+const webpack = require('webpack');
 const path = require('path');
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const EslingPlugin = require('eslint-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-const baseConfig = {
+const config = {
   entry: path.resolve(__dirname, './src/index'),
-  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
       },
       {
-        test: /\.ts$/i,
-        use: 'ts-loader',
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          },
+          'postcss-loader'
+        ]
       },
       {
-        test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
+        test: /\.svg$/,
+        use: 'file-loader'
+      }
+    ]
   },
   resolve: {
-    extensions: ['.ts', '.js'],
-    alias: {
-      images: path.resolve(__dirname, 'src/img/'),
-    },
-  },
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, '../dist'),
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
-      filename: 'index.html',
-      // favicon: './src/favicons/favicon32.png',
-    }),
-    new CleanWebpackPlugin(),
-    new EslingPlugin({ extensions: 'ts' }),
-  ],
+    new MiniCssExtractPlugin()
+  ]
 };
 
 module.exports = ({ mode }) => {
   const isProductionMode = mode === 'prod';
   const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+  // plugins: [new ESLintPlugin(options)];
 
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          from: 'src/img',
-        },
-      ],
-    }),
-  ];
-
-  return merge(baseConfig, envConfig);
+  return merge(config, envConfig);
 };
+
