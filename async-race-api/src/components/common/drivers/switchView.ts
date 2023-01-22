8b16f3e81000f49fd9dab5c40212d7b46e055a90
Irina Migunova <ista.irina@gmail.com>
2023-01-22 22:22:28 +0300

    feat: change way to switch between garage and winners

--- async-race-api/src/components/common/drivers/switchView.ts
+++ async-race-api/src/components/common/drivers/switchView.ts
@@ -1,9 +1,9 @@
-import GarageHTML from '../../garage/layout/garage';
+// import GarageHTML from '../../garage/layout/garage';
 import WinnersHTML from '../../winners/layout/winners';
 // import winnerCars from '../../winners/drivers/getWinners';
 // import { getPageNum } from '../../garage/drivers/valueCurrPageNumber';
 
-let saveStateInputs: string[] = [];
+// let saveStateInputs: string[] = [];
 
 function SwitchView(app: HTMLElement, buttonToGarage: HTMLButtonElement, buttonToWinners: HTMLButtonElement) {
   // const garage = document.getElementById('garage');
@@ -11,6 +11,11 @@ function SwitchView(app: HTMLElement, buttonToGarage: HTMLButtonElement, buttonT
   // console.log(winners);
 
   buttonToGarage.addEventListener('click', () => {
+    const garage = document.getElementById('garage');
+    const winners = document.getElementById('winners');
+    if (garage) {
+      garage.style.display = 'block';
+    }
     // console.log('switch to garage ' + getPageNum());
     // const garage = document.getElementById('garage');
     // const winners = document.getElementById('winners');
@@ -21,17 +26,20 @@ function SwitchView(app: HTMLElement, buttonToGarage: HTMLButtonElement, buttonT
     //   }
     // }
     if (app) {
-      const master: HTMLElement | null = document.querySelector('.master');
-      if (master) {
-        app.removeChild(master);
+      // const master: HTMLElement | null = document.querySelector('.master');
+      if (winners) {
+        app.removeChild(winners);
       }
-      app.appendChild(GarageHTML(saveStateInputs));
+      // app.appendChild(GarageHTML(saveStateInputs));
     }
   });
 
   buttonToWinners.addEventListener('click', () => {
-    //   const garage = document.getElementById('garage');
-    //   const winners = document.getElementById('winners');
+    const garage = document.getElementById('garage');
+    // const winners = document.getElementById('winners');
+    if (garage) {
+      garage.style.display = 'none';
+    }
     //   if (winners) {
     //     console.log('here is winners');
     //     winners.style.display = 'block';
@@ -45,17 +53,17 @@ function SwitchView(app: HTMLElement, buttonToGarage: HTMLButtonElement, buttonT
     //   }
     // });
     // console.log('winners');
-    const inpTextCreate = document.getElementById('inp__create-text') as HTMLInputElement;
-    const inpColorCreate = document.getElementById('inp__create-color') as HTMLInputElement;
-    const inpTextUpd = document.getElementById('inp__update-text') as HTMLInputElement;
-    const inpColorUpd = document.getElementById('inp__update-color') as HTMLInputElement;
-    saveStateInputs = [inpTextCreate.value, inpColorCreate.value, inpTextUpd.value, inpColorUpd.value];
+    // const inpTextCreate = document.getElementById('inp__create-text') as HTMLInputElement;
+    // const inpColorCreate = document.getElementById('inp__create-color') as HTMLInputElement;
+    // const inpTextUpd = document.getElementById('inp__update-text') as HTMLInputElement;
+    // const inpColorUpd = document.getElementById('inp__update-color') as HTMLInputElement;
+    // saveStateInputs = [inpTextCreate.value, inpColorCreate.value, inpTextUpd.value, inpColorUpd.value];
 
     if (app) {
-      const master: HTMLElement | null = document.querySelector('.master');
-      if (master) {
-        app.removeChild(master);
-      }
+      // const master: HTMLElement | null = document.querySelector('.master');
+      // if (master) {
+      //   app.removeChild(master);
+      // }
       app.appendChild(WinnersHTML());
       // winnerCars();
     }
